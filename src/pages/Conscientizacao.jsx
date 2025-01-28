3735cc26d70e5408a4af1339d569bf3a224ba341
Majucih <juhmarymendes@gmail.com>
2025-01-28 20:26:10 -0300

    Criação da pagina Conscientização

---
+++ src/pages/Conscientizacao.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Conscientizacao = () => {
+return (
+<main>
+<h2>Dicas Práticas de Economia de Energia</h2>
+<ul>
+<li>Desligue aparelhos da tomada quando não estiver usando.</li>
+<li>Aproveite a luz natural o máximo possível.</li>
+<li>Regule a temperatura do ar-condicionado corretamente.</li>
+<li>Evite banhos longos e reduza o uso do chuveiro elétrico.</li>
+<li>Prefira eletrodomésticos com selo de eficiência energética.</li>
+<li>Mantenha a borracha da geladeira em boas condições para evitar vazamento de ar.</li>
+</ul>
+<button>Acesse a Calculadora de Consumo</button>
+</main>
+);
+};
+
+export default Conscientizacao;
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import EMaisPage from "./pages/EMaisPage";
 import Footer from "./componets/Footer";
 import Simulador from "./pages/Simulador"
+import Conscientizacao from "./pages/Conscientizacao";
 
 const RoutesApp = ()=>{
     return(
@@ -11,6 +12,7 @@ const RoutesApp = ()=>{
     <Route path="*" element={<Home/>} />
     <Route path="/emais" element={<EMaisPage />} />
     <Route path="/simulador" element={<Simulador/>} />
+    <Route path="/conscientizacao" element={<Conscientizacao/>} />
 </Routes>
 <Footer/>
 </BrowserRouter>
